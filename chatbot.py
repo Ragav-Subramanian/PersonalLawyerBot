a63f92213fa1a100e97f39321e6a97558a31f8a5
Ragav Subramanian <ragavvijay35@gmail.com>
2020-06-14 16:22:28 +0530

    Initiallly trained

--- chatbot.py
+++ chatbot.py
@@ -22,10 +22,12 @@ from chatterbot.trainers import ListTrainer
 trainer = ListTrainer(chatbot)
 
 training_data_law100 = open('training_data/f100.txt',encoding="utf-8").read().splitlines()
+training_data_400 = open('training_data/400l.txt').read().splitlines()
+training_data_500 = open('training_data/ipcc.txt').read().splitlines()
 training_data_quesans = open('training_data/ques_ans.txt').read().splitlines()
 training_data_personal = open('training_data/personal_ques.txt').read().splitlines()
 
-training_data = training_data_law100 + training_data_quesans + training_data_personal
+training_data = training_data_law100 + training_data_400 + training_data_500 + training_data_quesans + training_data_personal
 
 trainer.train(training_data)
 
