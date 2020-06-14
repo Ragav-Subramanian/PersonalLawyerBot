c36ff43fa468b9820ac1a4e40c0026db7b38107b
Ragav Subramanian <ragavvijay35@gmail.com>
2020-06-15 03:25:40 +0530

    Initiallly trained

--- chatbot.py
+++ chatbot.py
@@ -5,12 +5,11 @@ chatbot = ChatBot(
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
     logic_adapters=[
         'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.TimeLogicAdapter',
         'chatterbot.logic.BestMatch',
         {
             'import_path': 'chatterbot.logic.BestMatch',
             'default_response': 'I am sorry, but I do not understand. ',
-            'maximum_similarity_threshold': 0.90
+            'maximum_similarity_threshold': .90
         }
     ],
     database_uri='sqlite:///database.sqlite3'
@@ -31,11 +30,3 @@ training_data = training_data_law100 + training_data_400 + training_data_500 + t
 
 trainer.train(training_data)
 
-# Training With Corpus
-from chatterbot.trainers import ChatterBotCorpusTrainer
-
-trainer_corpus = ChatterBotCorpusTrainer(chatbot)
-
-trainer_corpus.train(
-    'chatterbot.corpus.english'
-)
